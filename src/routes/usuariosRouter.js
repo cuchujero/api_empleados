15b63f184f1fa992830de95c1e656d9c2ce35e1a
cuchujero <cuchujero@hotmail.com>
2024-11-17 18:58:18 -0300

    first commit

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 const {usuarioFieldsValidator, IdValidator} = require('../helpers/validationsFields');
-const {generalValidation} = require('../middlewares/validationsMw');
 const {requestAuthorization} = require('../middlewares/authorizationRequestMw');
 
 router.get('/', requestAuthorization, usuariosController.getUsuarios);
-router.post('/', requestAuthorization, usuarioFieldsValidator, generalValidation, usuariosController.createProfesional);
+router.post('/', requestAuthorization, usuarioFieldsValidator, usuariosController.createProfesional);
 router.post('/login', requestAuthorization, usuariosController.loginUsuario);
-router.put('/', requestAuthorization, IdValidator, usuarioFieldsValidator, generalValidation, usuariosController.updateUsuario);
-router.delete('/', requestAuthorization, IdValidator, generalValidation, usuariosController.deleteUsuario);
+router.put('/', requestAuthorization, IdValidator, usuarioFieldsValidator, usuariosController.updateUsuario);
+router.delete('/', requestAuthorization, IdValidator, usuariosController.deleteUsuario);
 
 module.exports = router;
